3ef9546f1ca20032b0ca895678d5f327e02443c6
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2025-07-18 09:42:27 +0700

    Update Footbar.tsx

--- src/components/organisms/Footbar.tsx
+++ src/components/organisms/Footbar.tsx
@@ -14,7 +14,7 @@ export default function Footbar() {
             <span className="ml-3 text-xl">Tailblocks</span>
           </Link>
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            © 2020 Tailblocks —
+            © 2025 Rifqi Febrianto —
             <a
               href="https://x.com/Ryu13300696"
               className="text-gray-600 ml-1"
