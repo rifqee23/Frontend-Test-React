9ca0dc88b32dbb6f276a0df98ceebd2f8fef816d
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2025-07-18 09:38:47 +0700

    Update Footbar.tsx

--- src/components/organisms/Footbar.tsx
+++ src/components/organisms/Footbar.tsx
@@ -16,12 +16,12 @@ export default function Footbar() {
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
             © 2020 Tailblocks —
             <a
-              href="https://twitter.com/knyttneve"
+              href="https://x.com/Ryu13300696"
               className="text-gray-600 ml-1"
               rel="noopener noreferrer"
               target="_blank"
             >
-              @knyttneve
+              @rifqee23
             </a>
           </p>
           <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
